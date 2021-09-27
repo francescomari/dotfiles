c53b13dc2dfdc04f3a78d2015235d34f2d439fd8
Francesco Mari <mari.francesco@gmail.com>
2021-09-27 22:24:57 +0200

    Improve formatting

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 # Install Brew
+
 if ! command -v brew >/dev/null ; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Install Oh My ZSH!
+
 if [ ! -d ~/.oh-my-zsh ] ; then
     RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Install SDKMAN!
+
 if [ ! -d ~/.sdkman ] ; then
     curl -s "https://get.sdkman.io" | bash
 fi
@@ -58,6 +61,7 @@ for i in "${casks[@]}" ; do
 done
 
 # Stow configurations
+
 configs=(
     git
     gvim
