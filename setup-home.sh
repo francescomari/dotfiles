ce398b8d6b2d177abacb3dab9deff11e69f32c45
Francesco Mari <mari.francesco@gmail.com>
2022-03-15 23:05:59 +0100

    Decouple data and calls

--- setup-home.sh
+++ setup-home.sh
@@ -2,45 +2,50 @@
 
 source lib/setup.sh
 
-parse_flags "$@"
-
-require_homebrew
-
-install_taps \
+taps=(
     homebrew/cask-fonts
+)
 
-install_bottles \
-    git \
-    go \
-    jq \
-    shellcheck \
-    stow \
+bottles=(
+    git
+    go
+    jq
+    shellcheck
+    stow
     vim
+)
 
-install_casks \
-    alfred \
-    daisydisk \
-    docker \
-    font-jetbrains-mono \
-    google-chrome \
-    google-drive \
-    iterm2 \
-    jetbrains-toolbox \
-    rectangle \
-    signal \
-    slack \
-    spotify \
-    textual \
-    visual-studio-code \
+casks=(
+    alfred
+    daisydisk
+    docker
+    font-jetbrains-mono
+    google-chrome
+    google-drive
+    iterm2
+    jetbrains-toolbox
+    rectangle
+    signal
+    slack
+    spotify
+    textual
+    visual-studio-code
     whatsapp
+)
 
-install_configs \
-    git-base \
-    git-home \
-    sh-base \
-    sh-home \
+configs=(
+    git-base
+    git-home
+    sh-base
+    sh-home
     vim
+)
 
+parse_flags "$@"
+require_homebrew
+install_taps "${taps[@]}"
+install_bottles "${bottles[@]}"
+install_casks "${casks[@]}"
+install_configs "${configs[@]}"
 setup_omz
-
 print_errors_and_exit
--- setup-work.sh
+++ setup-work.sh
@@ -2,57 +2,61 @@
 
 source lib/setup.sh
 
-parse_flags "$@"
-
-require_homebrew
-
-install_taps \
-    homebrew/cask-fonts \
+taps=(
+    homebrew/cask-fonts
     ngrok/ngrok
+)
 
-install_bottles \
-    fnm \
-    git \
-    gnupg \
-    go \
-    jq \
-    ngrok \
-    pinentry-mac \
-    postgresql@13 \
-    redis \
-    shellcheck \
-    stow \
+bottles=(
+    fnm
+    git
+    gnupg
+    go
+    jq
+    ngrok
+    pinentry-mac
+    postgresql@13
+    redis
+    shellcheck
+    stow
     vim
+)
 
-install_casks \
-    alfred \
-    cmake \
-    daisydisk \
-    docker \
-    font-jetbrains-mono \
-    google-chrome \
-    google-drive \
-    iterm2 \
-    jetbrains-toolbox \
-    postico \
-    rectangle \
-    signal \
-    slack \
-    spotify \
-    textual \
-    visual-studio-code \
-    whatsapp \
+casks=(
+    alfred
+    cmake
+    daisydisk
+    docker
+    font-jetbrains-mono
+    google-chrome
+    google-drive
+    iterm2
+    jetbrains-toolbox
+    postico
+    rectangle
+    signal
+    slack
+    spotify
+    textual
+    visual-studio-code
+    whatsapp
     zoom
+)
 
-install_configs \
-    git-base \
-    git-work \
-    sh-base \
-    sh-work \
+configs=(
+    git-base
+    git-work
+    sh-base
+    sh-work
     vim
+)
 
+parse_flags "$@"
+require_homebrew
+install_taps "${taps[@]}"
+install_bottles "${bottles[@]}"
+install_casks "${casks[@]}"
+install_configs "${configs[@]}"
 setup_omz
-
 setup_gnupg
-
 print_errors_and_exit
