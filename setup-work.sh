fb5b02264c5b2a483bd3fac6054f5c974acfda16
Francesco Mari <mari@adobe.com>
2024-03-18 12:58:13 +0100

    Add Skyline CLI

--- setup-work.sh
+++ setup-work.sh
@@ -2,11 +2,42 @@
 
 source lib/setup.sh
 
+require_internal_tap() {
+    if brew tap | grep '^experience-platform/internal$' ; then
+        return
+    fi
+
+    info 'Installing the internal tap...'
+
+    if ! brew tap --force-auto-update experience-platform/homebrew-internal git@git.corp.adobe.com:experience-platform/homebrew-internal.git ; then
+        fatal 'Unable to install the internal tap'
+    fi
+}
+
+install_skyline_cli() {
+    if [ -d "$HOME/.sky" ] ; then
+        return
+    fi
+
+    info 'Installing the Skyline CLI...'
+
+    if ! git clone git@git.corp.adobe.com:experience-platform/skyline-cli.git ~/.sky ; then
+        fatal 'Unable to clone the Skyline CLI'
+    fi
+
+    ln -s "$HOME/.sky/bin/sky" "$HOME/bin/sky"
+}
+
 bottles=(
+    azure-cli
     azure/kubelogin/kubelogin
+    bash
     cloc
+    coreutils
     direnv
     dive
+    experience-platform/internal/cmcli
+    experience-platform/internal/skyline-ops-cli
     fnm
     fx
     git
@@ -19,11 +50,13 @@ bottles=(
     maven
     mkcert
     node
+    parallel
     shellcheck
     stow
     tree
     vim
     watch
+    yq
 )
 
 casks=(
@@ -60,6 +93,8 @@ configs=(
 
 parse_flags "$@"
 require_homebrew
+require_internal_tap
+install_skyline_cli
 install_bottles "${bottles[@]}"
 check_unmanaged_bottles "${bottles[@]}"
 install_casks "${casks[@]}"
