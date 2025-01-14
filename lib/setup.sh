033fa10b48c766a83d9a7db87b4a7f27b7b96926
Francesco Mari <mari@adobe.com>
2025-01-14 12:21:21 +0100

    Add NeoVim and LazyVim

--- lib/setup.sh
+++ lib/setup.sh
@@ -6,182 +6,199 @@
 exec 3>&1 4>&2 &>/dev/null
 
 info() {
-    echo >&3 "$@"
+  echo >&3 "$@"
 }
 
 warn() {
-    warnings+=("$1")
+  warnings+=("$1")
 }
 
 error() {
-    errors+=("$1")
+  errors+=("$1")
 }
 
 fatal() {
-    echo >&4 "$1"
-    exit 1
+  echo >&4 "$1"
+  exit 1
 }
 
 parse_flags() {
-    local verbose
+  local verbose
 
-    verbose=0
+  verbose=0
 
-    while [ $# -gt 0 ] ; do
-        case $1 in
-            -v)
-                verbose=1
-                shift
-                ;;
-            -*)
-                fatal "Unrecognized option '$1'"
-                ;;
-            *)
-                fatal "Unrecognized argument '$1'"
-                ;;
-        esac
-    done
+  while [ $# -gt 0 ]; do
+    case $1 in
+    -v)
+      verbose=1
+      shift
+      ;;
+    -*)
+      fatal "Unrecognized option '$1'"
+      ;;
+    *)
+      fatal "Unrecognized argument '$1'"
+      ;;
+    esac
+  done
 
-    if [ $verbose -eq 1 ] ; then
-        exec 1>&3 2>&4
-    fi
+  if [ $verbose -eq 1 ]; then
+    exec 1>&3 2>&4
+  fi
 }
 
 command_exists() {
-    command -v "$1" > /dev/null 2>&1
+  command -v "$1" >/dev/null 2>&1
 }
 
 commands_exist() {
-    local c
+  local c
 
-    for c in "$@" ; do
-        if ! command_exists "$c" ; then
-            return 1
-        fi
-    done
+  for c in "$@"; do
+    if ! command_exists "$c"; then
+      return 1
+    fi
+  done
 
-    return 0
+  return 0
 }
 
 require_homebrew() {
-    if ! command_exists brew ; then
-        fatal 'Install Homebrew first. Follow the instructions at https://brew.sh/'
-    fi
+  if ! command_exists brew; then
+    fatal 'Install Homebrew first. Follow the instructions at https://brew.sh/'
+  fi
 }
 
 install_tap_with_url() {
-    info "Installing Homebrew tap '$1'"
+  info "Installing Homebrew tap '$1'"
 
-    if ! brew tap "$1" "$2" ; then
-        error "Installation of Homebrew tap '$1' failed"
-    fi
+  if ! brew tap "$1" "$2"; then
+    error "Installation of Homebrew tap '$1' failed"
+  fi
 }
 
 install_bottles() {
-    info 'Installing Homebrew bottles...'
+  info 'Installing Homebrew bottles...'
 
-    for i in  "$@" ; do
-        if  brew list "$i" ; then
-            info "Homebrew bottle '$i' already installed"
-            continue
-        fi
+  for i in "$@"; do
+    if brew list "$i"; then
+      info "Homebrew bottle '$i' already installed"
+      continue
+    fi
 
-        info "Installing Homebrew bottle '$i'"
+    info "Installing Homebrew bottle '$i'"
 
-        if ! brew install "$i" ; then
-            error "Installation of Homebrew bottle '$i' failed"
-        fi
-    done
+    if ! brew install "$i"; then
+      error "Installation of Homebrew bottle '$i' failed"
+    fi
+  done
 }
 
 check_unmanaged_bottles() {
-    info 'Checking for unmanaged bottles...'
+  info 'Checking for unmanaged bottles...'
 
-    for i in $(brew leaves -r) ; do
-        for j in "$@" ; do
-            if [ "$i" == "$j" ] ; then
-                continue 2
-            fi
-        done
-
-        warn "Bottle '${i}' is not managed by configuration"
+  for i in $(brew leaves -r); do
+    for j in "$@"; do
+      if [ "$i" == "$j" ]; then
+        continue 2
+      fi
     done
+
+    warn "Bottle '${i}' is not managed by configuration"
+  done
 }
 
 install_casks() {
-    info 'Installing Homebrew casks...'
+  info 'Installing Homebrew casks...'
 
-    for i in "$@" ; do
-        if brew list --cask "$i" ; then
-            info "Homebrew cask '$i' already installed"
-            continue
-        fi
+  for i in "$@"; do
+    if brew list --cask "$i"; then
+      info "Homebrew cask '$i' already installed"
+      continue
+    fi
 
-        info "Installing Homebrew cask '$i'"
+    info "Installing Homebrew cask '$i'"
 
-        if ! brew install --cask "$i" ; then
-            error "Installation of Homebrew cask '$i' failed"
-        fi
-    done
+    if ! brew install --cask "$i"; then
+      error "Installation of Homebrew cask '$i' failed"
+    fi
+  done
 }
 
 check_unmanaged_casks() {
-    info 'Checking for unmanaged casks...'
+  info 'Checking for unmanaged casks...'
 
-    for i in $(brew list --cask) ; do
-        for j in "$@" ; do
-            if [ "$i" == "${j##*/}" ] ; then
-                continue 2
-            fi
-        done
-
-        warn "Cask '${i}' is not managed by configuration"
+  for i in $(brew list --cask); do
+    for j in "$@"; do
+      if [ "$i" == "${j##*/}" ]; then
+        continue 2
+      fi
     done
+
+    warn "Cask '${i}' is not managed by configuration"
+  done
 }
 
 install_configs() {
-    info 'Installing Stow configurations...'
+  info 'Installing Stow configurations...'
 
-    for i in "$@" ; do
-        info "Installing Stow configuration '$i'"
+  for i in "$@"; do
+    info "Installing Stow configuration '$i'"
 
-        if ! stow "$i" ; then
-            error "Installation of Stow configuration '$i' failed"
-        fi
-    done
+    if ! stow "$i"; then
+      error "Installation of Stow configuration '$i' failed"
+    fi
+  done
 }
 
 setup_omz() {
-    if [ -d ~/.oh-my-zsh ] ; then
-        info 'Oh My ZSH! already installed'
-        return
-    fi
+  if [ -d ~/.oh-my-zsh ]; then
+    info 'Oh My ZSH! already installed'
+    return
+  fi
 
-    info 'Installing Oh My ZSH!'
+  info 'Installing Oh My ZSH!'
 
-    if ! KEEP_ZSHRC=yes RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ; then
-        error 'Installation of Oh My ZSH! failed'
-    fi
+  if ! KEEP_ZSHRC=yes RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+    error 'Installation of Oh My ZSH! failed'
+  fi
 }
 
 print_errors_and_exit() {
-    if [ ${#warnings[@]} -gt 0 ] ; then
-        info "Warnings:"
+  if [ ${#warnings[@]} -gt 0 ]; then
+    info "Warnings:"
 
-        for w in "${warnings[@]}" ; do
-            info " * $w"
-        done
-    fi
+    for w in "${warnings[@]}"; do
+      info " * $w"
+    done
+  fi
 
-    if [ ${#errors[@]} -gt 0 ] ; then
-        info "Setup errors:"
+  if [ ${#errors[@]} -gt 0 ]; then
+    info "Setup errors:"
 
-        for e in "${errors[@]}" ; do
-            info " * $e"
-        done
+    for e in "${errors[@]}"; do
+      info " * $e"
+    done
 
-        exit 1
-    fi
+    exit 1
+  fi
 
-    exit 0
+  exit 0
+}
+
+install_lazyvim() {
+  if [ -d ~/.config/nvim ]; then
+    info 'LazyVim already installed'
+    return
+  fi
+
+  if ! git clone https://github.com/LazyVim/starter ~/.config/nvim; then
+    error 'Cloning LazyVim failed'
+    return 1
+  fi
+
+  if ! rm -rf ~/.config/nvim/.git; then
+    error 'Removing LazyVim git directory failed'
+    return 1
+  fi
 }
--- setup-work.sh
+++ setup-work.sh
@@ -3,113 +3,117 @@
 source lib/setup.sh
 
 require_internal_tap() {
-    if brew tap | grep '^experience-platform/internal$' ; then
-        return
-    fi
+  if brew tap | grep '^experience-platform/internal$'; then
+    return
+  fi
 
-    info 'Installing the internal tap...'
+  info 'Installing the internal tap...'
 
-    if ! brew tap --force-auto-update experience-platform/homebrew-internal git@git.corp.adobe.com:experience-platform/homebrew-internal.git ; then
-        fatal 'Unable to install the internal tap'
-    fi
+  if ! brew tap --force-auto-update experience-platform/homebrew-internal git@git.corp.adobe.com:experience-platform/homebrew-internal.git; then
+    fatal 'Unable to install the internal tap'
+  fi
 }
 
 install_skyline_cli() {
-    if [ -d "$HOME/.sky" ] ; then
-        return
-    fi
+  if [ -d "$HOME/.sky" ]; then
+    return
+  fi
 
-    info 'Installing the Skyline CLI...'
+  info 'Installing the Skyline CLI...'
 
-    if ! git clone git@git.corp.adobe.com:experience-platform/skyline-cli.git ~/.sky ; then
-        fatal 'Unable to clone the Skyline CLI'
-    fi
+  if ! git clone git@git.corp.adobe.com:experience-platform/skyline-cli.git ~/.sky; then
+    fatal 'Unable to clone the Skyline CLI'
+  fi
 
-    ln -s "$HOME/.sky/bin/sky" "$HOME/bin/sky"
+  ln -s "$HOME/.sky/bin/sky" "$HOME/bin/sky"
 }
 
 bottles=(
-    azure-cli
-    azure/kubelogin/kubelogin
-    bash
-    bats-core
-    cloc
-    conftest
-    coreutils
-    direnv
-    dive
-    experience-platform/internal/cmcli
-    experience-platform/internal/skyline-ops-cli
-    fd
-    fnm
-    fx
-    git
-    gitslave
-    gnu-sed
-    go
-    goreleaser
-    hashicorp/tap/vault
-    helm
-    jenv
-    jq
-    kind
-    kubeconform
-    kubectx
-    lazygit
-    maven
-    mkcert
-    mongodb-atlas-cli
-    node
-    operator-sdk
-    parallel
-    prometheus
-    python
-    python-tk@3.13
-    rbenv
-    ripgrep
-    ruby
-    shellcheck
-    stow
-    tree
-    vim
-    watch
-    yamllint
-    yq
+  azure-cli
+  azure/kubelogin/kubelogin
+  bash
+  bats-core
+  cloc
+  conftest
+  coreutils
+  direnv
+  dive
+  experience-platform/internal/cmcli
+  experience-platform/internal/skyline-ops-cli
+  fd
+  fnm
+  fx
+  fzf
+  git
+  gitslave
+  gnu-sed
+  go
+  goreleaser
+  hashicorp/tap/vault
+  helm
+  jenv
+  jq
+  kind
+  kubeconform
+  kubectx
+  lazygit
+  lua
+  luarocks
+  maven
+  mkcert
+  mongodb-atlas-cli
+  neovim
+  node
+  operator-sdk
+  parallel
+  prometheus
+  python
+  python-tk@3.13
+  rbenv
+  ripgrep
+  ruby
+  shellcheck
+  stow
+  tree
+  vim
+  watch
+  yamllint
+  yq
 )
 
 casks=(
-    1password
-    1password-cli
-    alfred
-    daisydisk
-    discord
-    docker
-    font-jetbrains-mono
-    font-jetbrains-mono-nerd-font
-    google-drive
-    iterm2
-    jetbrains-toolbox
-    mitmproxy
-    ngrok
-    obsidian
-    rectangle
-    signal
-    skype
-    spotify
-    textual
-    visual-studio-code
-    vlc
-    whatsapp
-    zoom
+  1password
+  1password-cli
+  alfred
+  daisydisk
+  discord
+  docker
+  font-jetbrains-mono
+  font-jetbrains-mono-nerd-font
+  google-drive
+  iterm2
+  jetbrains-toolbox
+  mitmproxy
+  ngrok
+  obsidian
+  rectangle
+  signal
+  skype
+  spotify
+  textual
+  visual-studio-code
+  vlc
+  whatsapp
+  zoom
 )
 
 configs=(
-    git-base
-    git-work
-    maven
-    sh-base
-    sh-work
-    vim
+  git-base
+  git-work
+  maven
+  sh-base
+  sh-work
+  vim
 )
 
 parse_flags "$@"
@@ -120,6 +124,7 @@ install_bottles "${bottles[@]}"
 check_unmanaged_bottles "${bottles[@]}"
 install_casks "${casks[@]}"
 check_unmanaged_casks "${casks[@]}"
+install_lazyvim
 install_configs "${configs[@]}"
 setup_omz
 print_errors_and_exit
